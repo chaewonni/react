1693327ab834662b036e9ce9a3e3cbf17023fe6e
chaewonkim <kimlove9182@gmail.com>
2025-01-06 03:37:02 +0900

    환경 변수 사용하여 민감 정보 제거

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params:{
-        api_key: "3a7a84032d3374e7805fea28cca7e210",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR"
     }
 });
